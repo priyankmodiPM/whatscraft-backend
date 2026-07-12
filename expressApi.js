be202da291fd65f3c97ea0a822e94ebf1f3d8dd0
Priyank Modi <pmodi@adobe.com>
2026-07-13 00:38:05 +0530

    feat: add mock Adobe Express API module

---
+++ expressApi.js
@@ -0,0 +1,25 @@
+const TEMPLATE_LAYERS = {
+  tpl_diwali: ['discount_text', 'headline', 'background_color'],
+  tpl_summer: ['headline', 'font_color'],
+  tpl_newarrival: ['headline'],
+};
+
+function getTemplateInfo(templateId) {
+  return {
+    templateId,
+    unlockedLayers: TEMPLATE_LAYERS[templateId] || [],
+  };
+}
+
+let renderRevision = 0;
+
+function applyEdit(templateId, currentEdits, newEdits) {
+  const mergedEdits = { ...currentEdits, ...newEdits };
+  renderRevision += 1;
+  return {
+    mergedEdits,
+    renderedImageUrl: `https://mock-express.local/render/${templateId}?rev=${renderRevision}`,
+  };
+}
+
+module.exports = { getTemplateInfo, applyEdit };
